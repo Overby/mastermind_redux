3c9574016461499fba03ef541e549489d6e20528
Andrew Overby <andrewdoverby@gmail.com>
2015-01-08 07:01:26 -0700

    add two rspec tests, more comments

--- gameplay.rb
+++ gameplay.rb
@@ -83,9 +83,9 @@ class Gameplay
     @turns += 1
   end
 
-  # def elapsed_time
-  #   end_time - start_time
-  #   #end_time.round.to_f(2) - start_time.round.to_f(2)
-  #
-  # end
+  def elapsed_time
+    end_time - start_time
+    #end_time.round.to_f(2) - start_time.round.to_f(2)
+  
+  end
 end
